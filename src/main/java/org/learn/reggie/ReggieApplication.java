f3c190f249b19024eab9f279cd4a8f1bc90b4022
KitKwok1012 <kwokjjj@gmail.com>
2023-04-14 22:42:10 +0800

    init

---
+++ src/main/java/org/learn/reggie/ReggieApplication.java
@@ -0,0 +1,14 @@
+package org.learn.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("------------------------------------- Started Reggie application -------------------------------------");
+    }
+}
---
+++ src/main/java/org/learn/reggie/config/WebMvcConfig.java
@@ -0,0 +1,19 @@
+package org.learn.reggie.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Slf4j
+@Configuration
+//2023-04-14
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry){
+        log.info("Start mapping static resource...");
+        registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
+        registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+    }
+}
