cd6b189f83c41f2f9d8d0e244b692962681ff2e0
KitKwok1012 <kwokjjj@gmail.com>
2023-04-16 17:25:13 +0800

    Implement Login filter logic

---
+++ src/main/java/org/learn/reggie/filter/LoginCheckFilter.java
@@ -0,0 +1,90 @@
+package org.learn.reggie.filter;
+
+/*
+Check user is login?
+ */
+
+
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.learn.reggie.common.R;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+@Slf4j
+public class LoginCheckFilter implements Filter {
+    public static final AntPathMatcher ANT_PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    /**
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        //1. Get URL
+        String requestURI = request.getRequestURI();
+
+        //2. Determine handle or not
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "front/**"
+        };
+
+        //3. Let it go
+        boolean check = check(urls, requestURI);
+        if (check) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //4. check is login
+        if(request.getSession().getAttribute("employee") != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        //5. not login yet
+        log.info("User not login, return to login page");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+    }
+
+    /**
+     *
+     * @param urls
+     * @param requestURI
+     * @return
+     */
+    public boolean check(String[] urls, String requestURI) {
+        for(String url: urls) {
+            boolean match = ANT_PATH_MATCHER.match(url, requestURI);
+            if (match) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
