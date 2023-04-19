629b8bcc70466280bd4df944cebe68dd8f22f35d
kitkwokjjj <kwokjjj@gmail.com>
2023-04-19 23:46:16 +0800

    Add entity, mapper, service, serviceimpl for dish

---
+++ src/main/java/org/learn/reggie/service/DishService.java
@@ -0,0 +1,7 @@
+package org.learn.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.learn.reggie.entity.Dish;
+
+public interface DishService extends IService<Dish> {
+}
