f1e8559beb2a44ecbadc60930651c18dc1a972e4
KitKwok1012 <kwokjjj@gmail.com>
2023-04-30 16:36:00 +0800

    implement list order detail function

---
+++ src/main/java/org/learn/reggie/dto/OrdersDto.java
@@ -0,0 +1,21 @@
+package org.learn.reggie.dto;
+
+import org.learn.reggie.entity.OrderDetail;
+import org.learn.reggie.entity.Orders;
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class OrdersDto extends Orders {
+
+    private String userName;
+
+    private String phone;
+
+    private String address;
+
+    private String consignee;
+
+    private List<OrderDetail> orderDetails;
+	
+}
