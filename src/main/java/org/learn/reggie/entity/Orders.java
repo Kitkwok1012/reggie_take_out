50272a37e087415c3ed24c5ba3a04119af2f08d3
KitKwok1012 <kwokjjj@gmail.com>
2023-04-30 15:17:55 +0800

    update order entity

--- src/main/java/org/learn/reggie/entity/Orders.java
+++ src/main/java/org/learn/reggie/entity/Orders.java
@@ -15,47 +15,39 @@ public class Orders implements Serializable {
 
     private Long id;
 
-    //订单号
     private String number;
 
-    //订单状态 1待付款，2待派送，3已派送，4已完成，5已取消
+    //1 Pending payment
+    //2 Pending deliver
+    //3 Delivered
+    //4 Finished
+    //5 Canceled
     private Integer status;
 
 
-    //下单用户id
     private Long userId;
 
-    //地址id
     private Long addressBookId;
 
-
-    //下单时间
     private LocalDateTime orderTime;
 
 
-    //结账时间
     private LocalDateTime checkoutTime;
 
-
-    //支付方式 1微信，2支付宝
+    //1. paypal
+    //2. credit care
     private Integer payMethod;
 
 
-    //实收金额
     private BigDecimal amount;
 
-    //备注
     private String remark;
 
-    //用户名
     private String userName;
 
-    //手机号
     private String phone;
 
-    //地址
     private String address;
 
-    //收货人
     private String consignee;
 }
