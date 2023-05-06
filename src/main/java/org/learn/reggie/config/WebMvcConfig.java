b702e60c873f4e1929e1378692f05cd6d52da3e9
KitKwok1012 <kwokjjj@gmail.com>
2023-05-06 16:25:31 +0800

    Add knife4j to enable swagger api doc

--- src/main/java/org/learn/reggie/config/WebMvcConfig.java
+++ src/main/java/org/learn/reggie/config/WebMvcConfig.java
@@ -1,17 +1,28 @@
 package org.learn.reggie.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import lombok.extern.slf4j.Slf4j;
 import org.learn.reggie.common.JacksonObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Slf4j
 @Configuration
+@EnableSwagger2
+@EnableKnife4j
 //2023-04-14
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
@@ -20,6 +31,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("Start mapping static resource...");
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
+        registry.addResourceHandler("doc.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
@@ -29,5 +42,23 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         //set object converter to use JackSon to serialize and deserialize
         messageConverter.setObjectMapper(new JacksonObjectMapper());
         converters.add(0, messageConverter);
+    }
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.learn.reggie.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Reggie take out")
+                .version("1.0")
+                .description("API doc")
+                .build();
     }
 }
